f623b51083ca1fa985f8f79cc204450bf0b2e41f
snigdha <sunny.rangineni@gmail.com>
2024-12-22 00:57:31 -0600

    creating user using post method

--- src/main/java/com/socialmedia/restful_web_services/user/UserDaoService.java
+++ src/main/java/com/socialmedia/restful_web_services/user/UserDaoService.java
@@ -12,12 +12,14 @@ public class UserDaoService {
 	
 	private static List<User> users = new ArrayList<>();
 	
+	private static int userCount = 0;
+	
 
 	static {
-		users.add(new User(1, "Adam", LocalDate.now().minusYears(30)));
-		users.add(new User(2, "Sofie", LocalDate.now().minusYears(20)));
-		users.add(new User(3, "Likki", LocalDate.now().minusYears(10)));
-		users.add(new User(4, "Tinku", LocalDate.now().minusYears(5)));
+		users.add(new User(++userCount, "Adam", LocalDate.now().minusYears(30)));
+		users.add(new User(++userCount, "Sofie", LocalDate.now().minusYears(20)));
+		users.add(new User(++userCount, "Likki", LocalDate.now().minusYears(10)));
+		users.add(new User(++userCount, "Tinku", LocalDate.now().minusYears(5)));
 	}
 	
 	public List<User> findAll() {
@@ -27,5 +29,11 @@ public class UserDaoService {
 	public User findOne(int id) {
 		Predicate<? super User> predicate = user -> user.getId().equals(id);
 		return users.stream().filter(predicate).findFirst().get();
+	}
+	
+	public User save(User user) {
+		user.setId(++userCount);
+		users.add(user);
+		return user;
 	}
 }
